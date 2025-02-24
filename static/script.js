2c578bea708d7cc488fca7424de678e019f958ea
djr812 <stillspirit@gmail.com>
2025-02-25 07:16:20 +1000

    Fixed isISSOverhead logic

--- static/script.js
+++ static/script.js
@@ -1,12 +1,10 @@
 // Global variables
-const x = document.getElementById("YourPos");
+const x = document.getElementById('YourPos');
 let myLat;
 let myLong;
 
-
 // Get location on page load
 getLocation();
-
 
 /**
  * Desc:    This function updates the date and time on the page.
@@ -19,10 +17,8 @@ function updateDateTime() {
     document.getElementById('datetime').innerText = now.toLocaleString();
 }
 
-
 // Update date and time every second
 setInterval(updateDateTime, 1000);
-
 
 /**
  * Desc:    This function gets the user's location and sends it to the server.
@@ -31,16 +27,15 @@ setInterval(updateDateTime, 1000);
  */
 function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+        x.innerHTML = 'Geolocation is not supported by this browser.';
     }
 }
 
-
 /**
  * Desc:    This function sends the user's location to the server.
- * @param   {Float} lat  
+ * @param   {Float} lat
  * @param   {Float} lon
  * @return  {} fetch('/update_location')
  */
@@ -48,50 +43,49 @@ function sendLocationToServer(lat, lon) {
     fetch('./update_location', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ lat: lat, lon: lon })
+        body: JSON.stringify({ lat: lat, lon: lon }),
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Location updated:', data);
-    })
-    .catch(error => {
-        console.error('Error updating location:', error);
-    });
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Location updated:', data);
+        })
+        .catch((error) => {
+            console.error('Error updating location:', error);
+        });
 }
-
 
 /**
  * Desc:    This function displays the user's position on the page.
  * @param   {Object} position
- * @return  {} x.innerHTML 
+ * @return  {} x.innerHTML
  */
 function showPosition(position) {
     myLat = position.coords.latitude;
     myLong = position.coords.longitude;
-    x.innerHTML = "Your Position is LAT: " + myLat + "  LONG: " + myLong;
+    x.innerHTML = 'Your Position is LAT: ' + myLat + '  LONG: ' + myLong;
     sendLocationToServer(myLat, myLong);
 }
 
-
 /**
- * Desc:    On Click this function sends a request to the server 
+ * Desc:    On Click this function sends a request to the server
  *          to refresh the ISS position.
  * @param   None
  * @return  {} fetch('/refresh_iss_position')
  */
 function refreshISSPosition() {
     fetch('./refresh_iss_position')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ISSPos').innerText = `ISS Position is LAT: ${data.iss_latitude}   LONG: ${data.iss_longitude}`;
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                'ISSPos'
+            ).innerText = `ISS Position is LAT: ${data.iss_latitude}   LONG: ${data.iss_longitude}`;
         })
-        .catch(error => {
+        .catch((error) => {
             console.error('Error fetching ISS position:', error);
         });
 }
-
 
 /**
  * Desc:    This function checks if the weather is clear.
@@ -106,7 +100,6 @@ function isClear() {
     }
 }
 
-
 /**
  * Desc:   This function checks if it is nighttime.
  * @param  None
@@ -120,20 +113,27 @@ function isNight() {
     }
 }
 
-
 /**
  * Desc:    This function checks if the ISS is overhead.
  * @param   None
  * @returns {boolean} True if the ISS is overhead, False otherwise.
  */
 function isISSOverhead() {
-    if (myLat - 5 <= iss_latitude <= myLat + 5 && myLong - 5 <= iss_longitude <= myLong + 5) {
+    console.log('myLat is ' + myLat);
+    console.log('myLong is ' + myLong);
+    console.log('iss_latitude is ' + iss_latitude);
+    console.log('iss_longitude is ' + iss_longitude);
+    if (
+        myLat - 5 <= iss_latitude &&
+        iss_latitude <= myLat + 5 &&
+        myLong - 5 <= iss_longitude &&
+        iss_longitude <= myLong + 5
+    ) {
         return true;
     } else {
         return false;
     }
 }
-
 
 /**
  * Desc:    This function updates the Go Look status on the page.
@@ -146,22 +146,36 @@ function updateGoLookStatus() {
     const sunsetTime = new Date();
     sunsetTime.setHours(sunsetHour, sunsetMinute, 0);
 
-    let goLook = "The weather is <i>" + weather_desc + "</i>, and its <b>Daytime!</b> You won't see the ISS yet. Wait for Nighttime.";
+    let goLook =
+        'The weather is <i>' +
+        weather_desc +
+        "</i>, and its <b>Daytime!</b> You won't see the ISS yet. Wait for Nighttime.";
     if (currentHour >= sunsetHour || currentHour <= sunriseHour) {
         if (isISSOverhead() && isClear()) {
-            goLook = "ISS is overhead and the weather is <i>" + weather_desc + "</i>. Go Look Up!";
-        } else if (isISSOverhead() && !isClear()) { 
-            goLook = "ISS is overhead but the weather is <i>" + weather_desc + "</i>. Not Tonight!";
+            goLook =
+                'ISS is overhead and the weather is <i>' +
+                weather_desc +
+                '</i>. Go Look Up!';
+        } else if (isISSOverhead() && !isClear()) {
+            goLook =
+                'ISS is overhead but the weather is <i>' +
+                weather_desc +
+                '</i>. Not Tonight!';
         } else if (!isISSOverhead() && isClear()) {
-            goLook = "ISS is not overhead but the weather is <i>" + weather_desc + "</i>, at least. Check Back Later!";
-        } else if (!isISSOverhead() && !isClear()) { 
-            goLook = "ISS is not overhead and the weather is <i>" + weather_desc + "</i>. Not Tonight!";
-        } 
+            goLook =
+                'ISS is not overhead but the weather is <i>' +
+                weather_desc +
+                '</i>, at least. Check Back Later!';
+        } else if (!isISSOverhead() && !isClear()) {
+            goLook =
+                'ISS is not overhead and the weather is <i>' +
+                weather_desc +
+                '</i>. Not Tonight!';
+        }
     }
 
     document.getElementById('goLookStatus').innerHTML = goLook;
 }
-
 
 // Update Go Look status on page load
 updateGoLookStatus();
